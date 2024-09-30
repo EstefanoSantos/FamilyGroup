ec4298267e903948de4eea55956a2506ef73c0a8
Estefano Santos <estefanosantosbarros@gmail.com>
2024-09-30 12:22:05 -0400

    Adicionando projeto springboot

---
+++ familygroup/src/main/java/com/familygroup/familygroup/FamilygroupApplication.java
@@ -0,0 +1,13 @@
+package com.familygroup.familygroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilygroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilygroupApplication.class, args);
+	}
+
+}
