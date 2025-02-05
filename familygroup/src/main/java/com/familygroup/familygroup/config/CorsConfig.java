1ac3fab3c4f75a59e016889e3ee0495162a6f0d1
Estefano Santos <estefanosantosbarros@gmail.com>
2025-02-05 09:12:53 -0300

    feat: adding cors configuration

---
+++ familygroup/src/main/java/com/familygroup/familygroup/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.familygroup.familygroup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.micrometer.common.lang.NonNull;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("null") @NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+    
+}
