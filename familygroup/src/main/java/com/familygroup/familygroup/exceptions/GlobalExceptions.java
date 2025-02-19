7e7f36be3dafc8ae7681d2b5f9531429d6a646e9
Estefano Santos <estefanosantosbarros@gmail.com>
2025-02-18 22:08:31 -0300

    refactor: reorganize imports

--- familygroup/src/main/java/com/familygroup/familygroup/exceptions/GlobalExceptions.java
+++ familygroup/src/main/java/com/familygroup/familygroup/exceptions/GlobalExceptions.java
@@ -1,15 +1,12 @@
 package com.familygroup.familygroup.exceptions;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import jakarta.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestControllerAdvice
@@ -17,12 +14,12 @@ public class GlobalExceptions {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> genericExceptionHandler(Exception ex) {
-        
+
         ApiError error = new ApiError(
-            List.of(ex.getLocalizedMessage()),
-            HttpStatus.CONFLICT.name(),
-            HttpStatus.CONFLICT.value(),
-            LocalDateTime.now());
+                List.of(ex.getLocalizedMessage()),
+                HttpStatus.CONFLICT.name(),
+                HttpStatus.CONFLICT.value(),
+                LocalDateTime.now());
 
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
